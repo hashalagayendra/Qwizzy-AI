a71edb9b3f31c2febc41fba9f72fc28beb091c98
Hasala Gayendra <140869934+hashalagayendra@users.noreply.github.com>
2025-12-05 21:30:54 +0530

    Update generative model to gemini-2.5-flashchange

--- src/app/api/geminai/route.js
+++ src/app/api/geminai/route.js
@@ -31,7 +31,7 @@ export async function POST(req) {
       try {
         console.log(data.prompt);
         const prompt = data.prompt;
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
